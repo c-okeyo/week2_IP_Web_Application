53f42b128d7cfab6acb70c9f038db39e0e33031e
c-okeyo <chrisokello26@gmail.com>
2022-10-13 22:38:47 +0300

    code formatting

--- js/main.js
+++ js/main.js
@@ -2,37 +2,25 @@ const DAYS_OF_THE_WEEK = ["Sunday","Monday","Tuesday", "Wednesday","Thurday", "F
 const MALE_NAMES = ["Kwasi","Kwadwo"," Kwabena","Kwaku", "Yaw","Kofi","Kwame"];
 const  FEMALE_NAMES = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
-var century= 19;
-var year=1991;
-var month =10; 
-var day = 3;
-var gender= "male";
-
-
+var year= parseInt(promt("Enter the year you were born"));
+var century= parseInt(year.toString.slice(0,2));
+var month =parseInt(promt("Enter the month you were born")); 
+var day = parseInt(promt("Enter the day you were born"));
+var gender= parseInt(promt("Enter your gender"));
 
 function nameAndDay(century,year,month,day,gender) {
 
     var indexOfTheDay = ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7
-    
-    indexOfTheDay = Math.round(indexOfTheDay);
-    
-    var dayTheUserWasBorn = DAYS_OF_THE_WEEK[indexOfTheDay];
-    
+    indexOfTheDay = Math.round(indexOfTheDay);    
+    var dayTheUserWasBorn = DAYS_OF_THE_WEEK[indexOfTheDay];    
     var nameOfTheUser = null; 
-
-    if (gender == "m"){
-        
-        nameOfTheUser = MALE_NAMES[indexOfTheDay]
-        
-    } else if (gender == "f"){
-        
-        nameOfTheUser = FEMALE_NAMES[indexOfTheDay]
-        
-    }else{
-        
+    if (gender == "m"){        
+        nameOfTheUser = MALE_NAMES[indexOfTheDay]        
+    } else if (gender == "f"){        
+        nameOfTheUser = FEMALE_NAMES[indexOfTheDay]        
+    }else{        
         alert ( "Please enter a valid gender")
     }
-
     var userInfo = [dayTheUserWasBorn,nameOfTheUser];
     return userInfo;
 }
