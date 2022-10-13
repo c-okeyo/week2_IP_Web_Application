07b3e713e450a185c75252bfbec37d7060ed5991
c-okeyo <chrisokello26@gmail.com>
2022-10-13 21:03:13 +0300

    business logic added

--- js/main.js
+++ js/main.js
@@ -2,4 +2,24 @@ const DAYS_OF_THE_WEEK = ["Sunday","Monday","Tuesday", "Wednesday","Thurday", "F
 const MALE_NAMES = ["Kwasi","Kwadwo"," Kwabena","Kwaku", "Yaw","Kofi","Kwame"];
 const  FEMALE_NAMES = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
-var indexOfTheDay = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+var century= 19;
+var year=;
+var month =; 
+var day = ;
+var gender= ;
+
+var indexOfTheDay = ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7
+indexOfTheDay = Math.round(indexOfTheDay);
+var dayTheUserWasBorn = DAYS_OF_THE_WEEK[indexOfTheDay];
+
+var nameOfTheUser = null; 
+
+if (gender == "m"){
+    nameOfTheUser = MALE_NAMES[indexOfTheDay]
+} else if (gender == "f"){
+    nameOfTheUser =FEMALE_NAMES
+}else{
+    alert ( "Please enter a valid gender")
+}
+
+console.log (nameOfTheUser)
