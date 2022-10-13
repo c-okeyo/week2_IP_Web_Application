fa7e1af531d49cb4aa72ee879cab500c6d248e87
c-okeyo <chrisokello26@gmail.com>
2022-10-13 20:20:05 +0300

    Constants defined

---
+++ js/main.js
@@ -0,0 +1,3 @@
+const DAYS_OF_THE_WEEK = ["Sunday","Monday","Tuesday", "Wednesday","Thurday", "Friday","Saturday"]
+const MALE_NAMES = ["Kwasi","Kwadwo"," Kwabena","Kwaku", "Yaw","Kofi","Kwame"]
+const  FEMALE_NAMES = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
